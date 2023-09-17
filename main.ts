a4fdd8abb99ca32edb4903d91f949bf07e5fc1eb
Jordany <luisjordany@outlook.es>
2023-09-17 14:32:23 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.House)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.showIcon(IconNames.Sad)
+basic.pause(100)
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+	
+})
